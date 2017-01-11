7bf691e9f98d5a63d1937f44a6eb484ca5594ddc
Karol Walasek <coreconviction@gmail.com>
2017-01-11 10:00:07 +0100

    Android deployment fixes

--- planet_shader.lua
+++ planet_shader.lua
@@ -7,8 +7,10 @@ return love.graphics.newShader([[
 	uniform vec2 dirToSun;
 	vec4 effect(vec4 color, Image texture, vec2 tex_co, vec2 co){
 		vec4 texcolor = Texel(sphere_normal, tex_co);
-		number d = dot((texcolor.xyz*2-vec3(1,1,1)), vec3(dirToSun,0));
-		d = clamp(d,0.1,1);
+		number d = dot((texcolor.xyz*2.0-vec3(1.0,1.0,1.0)), vec3(dirToSun,0));
+		if(d < 0.1){d = 0.1;}
+		if(d > 1.0){d = 1.0;}
+		//d = clamp(d,0.1,1);
 		
 		vec2 center = vec2(0.5,0.5);
 		vec2 toc = tex_co-center;
